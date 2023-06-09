80bf912c17e0209e9a73a186e76866026b9ff779
Qasim <mq12113@gmail.com>
2023-06-09 22:51:24 +0100

    Added burger button for mobile use

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css"
+import "../styles/Navbar.css";
+import ReorderIcon from '@mui/icons-material/Reorder'
+
 
 function Navbar() {
-    return <div className="navbar"> 
+    const [expandNavbar, setExpandNavbar] = React.useState(false);
+
+    return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}> 
     <div className="toggleButton">  
-    <button> </button>
+    <button 
+    onClick={() => {
+        setExpandNavbar((prev) => !prev);
+        }}
+        > 
+        <ReorderIcon />
+        </button>
     </div> 
     <div className="links"> 
     <Link to="/"> Home </Link>
@@ -14,6 +25,8 @@ function Navbar() {
 
     </div>
     </div>
+
+    );
 }
 
 
