48a9d3a0acf7637244cd37a912371c841a70cf15
Qasim <mq12113@gmail.com>
2023-06-10 20:48:01 +0100

    Added Home Page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import "../styles/Home.css"
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="home"> 
+        <div className="about"> 
+        <h2> Hi, My Name is Qasim </h2>
+        <div className="prompt"> 
+            <p>
+            A Web Developer with passion for learning and coding.
+            </p> 
+            <div className="socialMedia">
+            <LinkedInIcon />
+            <EmailIcon />
+            <GitHubIcon />
+            </div>
+            </div>
+        </div>
+        <div className="skills"> 
+        <h1> Skills </h1>
+        <ul className="list">
+            <li className="item"> 
+            <h2> Front-End </h2> 
+            <span> ReactJS, JavaScript, HTML, CSS, Bootstrap, MaterialUI, ES6, GitHub, TypeScript, jQuery, Node.</span>
+            </li>
+            <li className="item"> 
+            <h2> Additional Skills </h2> 
+            <span> Photoshop, Illustrator, InDesign, Sketch, Figma, XD, </span>
+            </li>
+
+        </ul>
+        </div>
+        </div>
     )
 }
 
